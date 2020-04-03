43fb01ff64a6d97864085d00d036fda0405f7bf1
Hardeep Singh Pahwa <hardeepsingh3485.in@gmail.com>
2020-04-03 17:51:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pahwa/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pahwa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
